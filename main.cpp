585d8a4f841627faa49551dfef499b32ca53816c
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-08-10 21:16:36 +0200

    Draw tictactoe grid

--- main.cpp
+++ main.cpp
@@ -2,13 +2,37 @@
 
 #include "test.hpp"
 
-constexpr int WIDTH = 640;
-constexpr int HEIGHT = 480;
+constexpr unsigned int WIDTH = 2560/3;
+constexpr unsigned int HEIGHT = 1440;
+
+void render(sf::RenderWindow& window) {
+    sf::Vertex hline[] = 
+    {
+        sf::Vertex(sf::Vector2f(0.f, static_cast<float>(HEIGHT)/3.f), sf::Color::Red),
+        sf::Vertex(sf::Vector2f(WIDTH, static_cast<float>(HEIGHT)/3.f), sf::Color::Red)
+    };
+    window.draw(hline, 2, sf::Lines);
+
+    hline[0].position.y += static_cast<float>(HEIGHT)/3.f;
+    hline[1].position.y += static_cast<float>(HEIGHT)/3.f;
+    window.draw(hline, 2, sf::Lines);
+
+
+    sf::Vertex vline[] = 
+    {
+        sf::Vertex(sf::Vector2f(static_cast<float>(WIDTH)/3.f, 0.f), sf::Color::Red),
+        sf::Vertex(sf::Vector2f(static_cast<float>(WIDTH)/3.f, HEIGHT), sf::Color::Red)
+    };
+    window.draw(vline, 2, sf::Lines);
+
+    vline[0].position.x += static_cast<float>(WIDTH)/3.f;
+    vline[1].position.x += static_cast<float>(WIDTH)/3.f;
+    window.draw(vline, 2, sf::Lines);
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), getTitle());
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    
 
     while (window.isOpen()) {
         sf::Event event;
@@ -17,9 +41,8 @@ int main() {
                 window.close();
             }
         }
-
         window.clear(sf::Color::White);
-        window.draw(shape);
+        render(window);
         window.display();
     }
 
