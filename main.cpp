a9c57c3a4eb064ddf403f46c6f604bf4523a77af
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-08-13 00:54:48 +0200

    Implement click on cell

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 #include "Renderer.hpp"
 #include "Game.hpp"
 
+#include <iostream>
+
+void clickOnCell(sf::RenderWindow& window) {
+    sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+
+    int cellX = localPosition.x/Renderer::CELL_WIDTH;
+    int cellY = localPosition.y/Renderer::CELL_HEIGHT;
+}
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(Renderer::WIDTH, Renderer::HEIGHT), 
                             Renderer::TITLE);
@@ -17,6 +26,10 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 game.state = Game::State::QUIT;
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                clickOnCell(window);
             }
         }
         renderer.Render(game.GetBoard());
