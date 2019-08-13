50f90a7f46874332976e32902523aed1facf04d2
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-08-13 03:51:13 +0200

    Fix some win check behaviour

--- Game.cpp
+++ Game.cpp
@@ -30,26 +30,24 @@ void Game::SetCell(Cell cell, int x, int y) {
 }
 
 void Game::CheckWin() {
-    if (moves == 9) {
-        state = State::TIE;
-        return;
-    }
     if (moves < 5) return;
 
-    if ((_board[0][0] == _board[0][1] && _board[0][1] == _board[0][2])
-        || (_board[1][0] == _board[1][1] && _board[1][1] == _board[1][2])
-        || (_board[2][0] == _board[2][1] && _board[2][1] == _board[2][2])
-        || (_board[0][0] == _board[1][0] && _board[1][0] == _board[2][0])
-        || (_board[1][0] == _board[1][1] && _board[1][1] == _board [1][2])
-        || (_board[2][0] == _board[2][1] && _board[2][1] == _board[2][2])
-        || (_board[0][0] == _board[1][1] && _board[1][1] == _board[2][2])
-        || (_board[0][2] == _board[1][1] && _board[1][1] == _board[2][0])
+    if ((_board[0][0] != EMPTY && _board[0][0] == _board[0][1] && _board[0][1] == _board[0][2])
+        || (_board[1][0] != EMPTY && _board[1][0] == _board[1][1] && _board[1][1] == _board[1][2])
+        || (_board[2][0] != EMPTY && _board[2][0] == _board[2][1] && _board[2][1] == _board[2][2])
+        || (_board[0][0] != EMPTY && _board[0][0] == _board[1][0] && _board[1][0] == _board[2][0])
+        || (_board[1][0] != EMPTY && _board[1][0] == _board[1][1] && _board[1][1] == _board [1][2])
+        || (_board[2][0] != EMPTY && _board[2][0] == _board[2][1] && _board[2][1] == _board[2][2])
+        || (_board[0][0] != EMPTY && _board[0][0] == _board[1][1] && _board[1][1] == _board[2][2])
+        || (_board[0][2] != EMPTY && _board[0][2] == _board[1][1] && _board[1][1] == _board[2][0])
     ) {
         if (player == Cell::X) {
             state = State::X_WON;
-        } else {
+        } else if (player == Cell::O) {
             state = State::O_WON;
         }
+    } else if (moves == 9) {
+        state = State::TIE;
     }
 }
 
